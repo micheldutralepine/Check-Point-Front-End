37dc91c4fadaf943603ada6bd63490b0762bbf48
guicvsub <rm552321@fiap.com.br>
2024-03-25 19:41:10 -0300

    adicionado-defer-no-js

--- javaScript.js
+++ javaScript.js
@@ -14,3 +14,48 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+//------------------FUNCAO FAVORITOS------------------------
+const favoritosBtn = document.querySelector('#favoritar');
+const iconePath = document.querySelector('#icone path');
+
+const toggleFavorito = () => {
+    let dadosImovel = obtervalores();
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    let index = favoritos.findIndex(item => item.titulo === dadosImovel.titulo); // Procura o índice do item na lista de favoritos
+
+    if (index !== -1) {
+        favoritos.splice(index, 1); // Remove o item se ele estiver na lista
+        console.log('Imóvel removido dos favoritos:', dadosImovel);
+        
+    } else {
+        favoritos.push(dadosImovel); // Adiciona o item se ele não estiver na lista
+    }
+
+    // Adiciona a classe se o favorito já estiver na lista
+    if (index !== -1) {
+        iconePath.classList.add('favorited');
+    } else {
+        iconePath.classList.remove('favorited');
+    }
+
+    localStorage.setItem('favoritos', JSON.stringify(favoritos)); // Salva a lista atualizada no localStorage
+};
+
+favoritosBtn.addEventListener('click', toggleFavorito);
+
+const obtervalores = () => {
+    const titulo = document.querySelector("#titulo").textContent;
+    const avaliacao = document.querySelector("#avaliacao").textContent;
+    const qtdComodos = document.querySelector("#comodos").textContent;
+    const preco = document.querySelector("#preco").textContent;
+    return { titulo, avaliacao, qtdComodos, preco };
+};
+
+// Adiciona a classe inicial se o favorito já estiver na lista
+let dadosImovel = obtervalores();
+let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+let index = favoritos.findIndex(item => item.titulo === dadosImovel.titulo);
+
+if (index !== -1) {
+    iconePath.classList.add('favorited');
+}
